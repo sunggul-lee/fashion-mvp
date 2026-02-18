14a4ae78f91a871b908b8a2c16efe5b1b3294ea3
sunggul-lee <gamblee1987@gmail.com>
2026-02-18 22:24:43 +0900

    버그수정: axios import

--- fashion-mvp-server/src/index.js
+++ fashion-mvp-server/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const { createClient } = require('@supabase/supabase-js');
