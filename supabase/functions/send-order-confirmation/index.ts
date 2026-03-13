da0f512463186962faf37665a4b0f97115dfe8a8
sunggul-lee <gamblee1987@gmail.com>
2026-03-13 22:22:18 +0900

    주문 완료 메일 배포 버그수정

--- supabase/functions/send-order-confirmation/index.ts
+++ supabase/functions/send-order-confirmation/index.ts
@@ -4,6 +4,8 @@ const RESEND_API_KEY = Deno.env.get('RESEND_API_KEY')
 serve(async (req) => {
   const { record } = await req.json() // Webhook에서 보낸 주문 데이터
 
+  console.log("수신된 주문 데이터:", record);
+
   const response = await fetch('https://api.resend.com/emails', {
     method: 'POST',
     headers: {
@@ -13,7 +15,7 @@ serve(async (req) => {
     body: JSON.stringify({
       from: 'onboarding@resend.dev',
       to: record.user_email,
-      subject: `[주문완료] ${record.order_number} 주문이 접수되었습니다.`,
+      subject: `[주문완료] ${record.id} 주문이 접수되었습니다.`,
       html: `<h1>주문해주셔서 감사합니다!</h1><p>총 결제 금액: ${record.total_price}원</p>`,
     }),
   })
